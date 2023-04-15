dddd2051a7d2fd7dbf7d3fcd813f3eff68a85511
mavlonovo <mavlonovo@tamu.edu>
2023-04-14 22:17:57 -0500

    final_commit

--- leyk-csce221-assignment-priority-queue-main/src/PriorityQueue.h
+++ leyk-csce221-assignment-priority-queue-main/src/PriorityQueue.h
@@ -5,13 +5,14 @@
 #include <vector>
 
 template <class T, class Container = std::vector<T>, class Compare = std::less<T>>
-class PriorityQueue {
+class PriorityQueue
+{
 public:
     using value_compare = Compare;
     using value_type = T;
     using size_type = size_t;
-    using reference = T&;
-    using const_reference = const T&;
+    using reference = T &;
+    using const_reference = const T &;
 
 private:
     Container c;
@@ -23,108 +24,171 @@ private:
     bool is_internal(size_t index) { return left_child(index) < c.size(); }
     bool is_leaf(size_t index) { return left_child(index) >= c.size(); }
 
-    
     /**
      * @brief Recursively (or Iteratively) swap the value at index up the heap until it is in the correct position.
-     * 
+     *
      * @note This is a max heap, so assume Compare is less, but promote the larger value.
-     * 
+     *
      * Used by push
-     * 
+     *
      * O(log(size()))
-     * 
+     *
      * @param index the current position to swap upwards
      */
-    void upheap(size_type index) {
+    void upheap(size_type index)
+    {
         // TODO
+        if (index != 0 && comp(c[parent(index)], c[index]))
+        {
+            T temp = std::move(c[parent(index)]);
+            c[parent(index)] = std::move(c[index]);
+            c[index] = std::move(temp);
+            upheap(parent(index));
+        }
     }
-    
+
     /**
      * @brief Recursively swap the value at index down the heap until it is in the correct position.
-     * 
+     *
      * @note This is a max heap, so assume Compare is less, so promote the larger value.
-     * 
+     *
      * Used by pop
-     * 
+     *
      * O(log(size()))
-     * 
+     *
      * @param index the current position to swap downwards
      */
-    void downheap(size_type index) {
+    void downheap(size_type index)
+    {
         // TODO
+        if (left_child(index) >= c.size() && right_child(index) >= c.size())
+            return;
+        // left and right
+        else if (right_child(index) >= c.size() && left_child(index) < c.size())
+        {
+            if (comp(c[index], c[left_child(index)]))//compare both
+            {
+                T temp = std::move(c[left_child(index)]);
+                c[left_child(index)] = std::move(c[index]);
+                c[index] = std::move(temp);
+            }
+        }
+        // Left and right not
+        else if (right_child(index) < c.size() && left_child(index) < c.size())
+        {
+            if (comp(c[left_child(index)], c[right_child(index)]))//compare both
+            {
+                if (comp(c[index], c[right_child(index)]))//compare again
+                {
+                    T temp = std::move(c[right_child(index)]);
+                    c[right_child(index)] = std::move(c[index]);
+                    c[index] = std::move(temp);
+                    downheap(right_child(index));
+                }
+            }
+            else//remaining case
+            {
+                if (comp(c[index], c[left_child(index)]))//compare again
+                {
+                    T temp = std::move(c[left_child(index)]);
+                    c[left_child(index)] = std::move(c[index]);
+                    c[index] = std::move(temp);
+                    downheap(left_child(index));
+                }
+            }
+        }
     }
 
 public:
     PriorityQueue() = default;
-    PriorityQueue( const PriorityQueue& other ) = default;
-    PriorityQueue( PriorityQueue&& other ) = default;
+    PriorityQueue(const PriorityQueue &other) = default;
+    PriorityQueue(PriorityQueue &&other) = default;
     ~PriorityQueue() = default;
-    PriorityQueue& operator=( const PriorityQueue& other ) = default;
-    PriorityQueue& operator=( PriorityQueue&& other ) = default;
+    PriorityQueue &operator=(const PriorityQueue &other) = default;
+    PriorityQueue &operator=(PriorityQueue &&other) = default;
 
     /**
      * @brief Return a const reference to the element at the top of the heap.
-     * 
+     *
      * O(1)
-     * 
+     *
      * @return const_reference to the element at the top of the heap.
      */
-    const_reference top() const { /* TODO */ }
+    const_reference top() const
+    { /* TODO */
+        return c[0];
+    }
 
     /**
      * @brief Return whether the heap is empty. This is the same as whether the underlying container, c, is empty.
-     * 
+     *
      * O(1)
-     * 
+     *
      * @return true c is empty
      * @return false c is not empty
      */
-    bool empty() const { /* TODO */ }
+    bool empty() const
+    { /* TODO */
+        return c.size() == 0;
+    }
 
     /**
      * @brief Return the number of elements in the heap. This is the same as the number of elements in the underlying container, c.
-     * 
+     *
      * O(1)
-     * 
+     *
      * @return size_type of the number of elements in the heap
      */
-    size_type size() const { /* TODO */ }
-	
-    /**
-     * @brief Inserts element and sorts the underlying container, c.
-     * 
-     * Uses upheap
-     * 
-     * O(log(size())) 
-     * 
-     * @param value inserted by copying into c 
-     */
-    void push( const value_type& value ) {
-        // TODO
+    size_type size() const
+    { /* TODO */
+        return c.size();
     }
 
     /**
      * @brief Inserts element and sorts the underlying container, c.
-     * 
+     *
      * Uses upheap
-     * 
-     * O(log(size())) 
-     * 
-     * @param value inserted by moving into c 
+     *
+     * O(log(size()))
+     *
+     * @param value inserted by copying into c
      */
-	void push( value_type&& value ) {
+    void push(const value_type &value)
+    {
         // TODO
+        c.push_back(value);
+        upheap(c.size() - 1);
+    }
+
+    /**
+     * @brief Inserts element and sorts the underlying container, c.
+     *
+     * Uses upheap
+     *
+     * O(log(size()))
+     *
+     * @param value inserted by moving into c
+     */
+    void push(value_type &&value)
+    {
+        // TODO
+        c.push_back(std::move(value));
+        upheap(c.size() - 1);
     }
 
     /**
      * @brief Removes the top element.
-     * 
+     *
      * Uses downheap
-     * 
-     * O(log(size())) 
-     * 
+     *
+     * O(log(size()))
+     *
      */
-    void pop() {
+    void pop()
+    {
         // TODO
+        c[0] = std::move(c[c.size() - 1]);
+        c.pop_back();
+        downheap(c[0]);
     }
 };
